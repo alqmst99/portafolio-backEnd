ee0d67e379e83ffba35cc9d87e7d3642d94ab72e
Nahuel Pierini <nahuel.pierini90@hotmail.com>
2022-10-29 14:20:42 -0300

    BackEnd-Portafolio 5

--- src/main/java/com/Pierini/Portafolio/PortafolioApplication.java
+++ src/main/java/com/Pierini/Portafolio/PortafolioApplication.java
@@ -31,7 +31,7 @@ public class PortafolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://frontend-portafolio-pierini.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
